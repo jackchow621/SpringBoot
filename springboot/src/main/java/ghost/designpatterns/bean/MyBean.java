5d13f259c013153801a161915ec2ff787442d312
jack <jack_chow621@sina.com>
2017-11-01 15:38:51 +0800

    将配置文件的属性赋给实体类
    将配置文件的属性赋给实体类

---
+++ springboot/src/main/java/ghost/designpatterns/bean/MyBean.java
@@ -0,0 +1,80 @@
+package ghost.designpatterns.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+@ConfigurationProperties(prefix="my")
+@Component
+public class MyBean {
+	private String name;
+    private int age;
+    private int number;
+    private String uuid;
+    private int max;
+    private String value;
+    private String greeting;
+    
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("MyBean [name=");
+		builder.append(name);
+		builder.append(", age=");
+		builder.append(age);
+		builder.append(", number=");
+		builder.append(number);
+		builder.append(", uuid=");
+		builder.append(uuid);
+		builder.append(", max=");
+		builder.append(max);
+		builder.append(", value=");
+		builder.append(value);
+		builder.append(", greeting=");
+		builder.append(greeting);
+		builder.append("]");
+		return builder.toString();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public int getMax() {
+		return max;
+	}
+	public void setMax(int max) {
+		this.max = max;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	public String getGreeting() {
+		return greeting;
+	}
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+    
+    
+}
--- springboot/src/main/java/ghost/designpatterns/controller/MyController.java
+++ springboot/src/main/java/ghost/designpatterns/controller/MyController.java
@@ -1,19 +1,26 @@
 package ghost.designpatterns.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import ghost.designpatterns.bean.MyBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableConfigurationProperties({MyBean.class})
 public class MyController {
 	
-	@Value("${my.name}")
-	private String name;
-	@Value("${my.age}")
-	private int age;
+//	@Value("${my.name}")
+//	private String name;
+//	@Value("${my.age}")
+//	private int age;
+	@Autowired
+	MyBean myBean;
 	
 	@RequestMapping(value = "/jack")
     public String jack(){
-        return name+":"+age;
+//        return name+":"+age;
+		return myBean.getGreeting()+" >>>>"+myBean.getName()+" >>>>"+ myBean.getUuid()+" >>>>"+myBean.getMax();
     }
 }
