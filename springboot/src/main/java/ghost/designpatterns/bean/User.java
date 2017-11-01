20c2e1cbf1e2f091f0a2b7859444150f1bfa5794
jack <jack_chow621@sina.com>
2017-11-01 15:49:52 +0800

    自定义配置文件
    自定义配置文件

---
+++ springboot/src/main/java/ghost/designpatterns/bean/User.java
@@ -0,0 +1,36 @@
+package ghost.designpatterns.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:test.properties")
+@ConfigurationProperties(prefix = "com.ghost")
+public class User {
+	private String name;
+	private int age;
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [name=");
+		builder.append(name);
+		builder.append(", age=");
+		builder.append(age);
+		builder.append("]");
+		return builder.toString();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
--- springboot/src/main/java/ghost/designpatterns/controller/MyController.java
+++ springboot/src/main/java/ghost/designpatterns/controller/MyController.java
@@ -1,6 +1,7 @@
 package ghost.designpatterns.controller;
 
 import ghost.designpatterns.bean.MyBean;
+import ghost.designpatterns.bean.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableConfigurationProperties({MyBean.class})
+@EnableConfigurationProperties({MyBean.class,User.class})
 public class MyController {
 	
 //	@Value("${my.name}")
@@ -17,10 +18,16 @@ public class MyController {
 //	private int age;
 	@Autowired
 	MyBean myBean;
+	@Autowired
+	User user;
 	
 	@RequestMapping(value = "/jack")
     public String jack(){
 //        return name+":"+age;
 		return myBean.getGreeting()+" >>>>"+myBean.getName()+" >>>>"+ myBean.getUuid()+" >>>>"+myBean.getMax();
     }
+	@RequestMapping(value = "/ghost")
+	public String ghost(){
+        return user.getName()+":"+user.getAge();
+	}
 }
