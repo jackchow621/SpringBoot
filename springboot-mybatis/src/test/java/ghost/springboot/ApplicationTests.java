ff025ad2116bee947d571cc80bb83fb81520ef62
jack <jack_chow621@sina.com>
2017-11-04 20:52:00 +0800

    SpringBoot集成redis
    SpringBoot集成redis

---
+++ springboot-mybatis/src/test/java/ghost/springboot/ApplicationTests.java
@@ -0,0 +1,31 @@
+package ghost.springboot;
+
+import ghost.springboot.dao.RedisDao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+    public static Logger logger= LoggerFactory.getLogger(ApplicationTests.class);
+    @Test
+    public void contextLoads() {
+    }
+
+    @Autowired
+    RedisDao redisDao;
+    @Test
+    public void testRedis(){
+        redisDao.setKey("name","jack");
+        redisDao.setKey("age","24");
+        logger.info(redisDao.getValue("name"));
+        logger.info(redisDao.getValue("age"));
+    }
+}
