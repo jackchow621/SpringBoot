f1f198176412882704ffa40852142b1c8c17a193
jack <jack_chow621@sina.com>
2017-11-05 19:35:19 +0800

    使用spring Restdocs创建API文档
    使用spring Restdocs创建API文档

--- springboot-mybatis/src/main/java/ghost/springboot/Application.java
+++ springboot-mybatis/src/main/java/ghost/springboot/Application.java
@@ -1,18 +1,13 @@
 package ghost.springboot;
 
-import ghost.springboot.dao.CustomerRepository;
-import ghost.springboot.entity.Customer;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application /*implements CommandLineRunner*/ {
 
-	@Autowired
-	private CustomerRepository repository;
+//	@Autowired
+//	private CustomerRepository repository;
 
 	/**
 	 * @param args
@@ -22,38 +17,38 @@ public class Application implements CommandLineRunner {
 		SpringApplication.run(Application.class, args);
 	}
 
-	public void run(String... arg0) throws Exception {
-		// TODO Auto-generated method stub
-		repository.deleteAll();
-
-		// save a couple of customers
-		repository.save(new Customer("Alice", "Smith"));
-		repository.save(new Customer("Bob", "Smith"));
-		repository.save(new Customer("Jack", "Chow"));
-
-		// fetch all customers
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (Customer customer : repository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println();
-
-		// fetch an individual customer
-		System.out.println("Customer found with findByFirstName('Alice'):");
-		System.out.println("--------------------------------");
-		System.out.println(repository.findByFirstName("Alice"));
-
-		System.out.println("Customers found with findByLastName('Smith'):");
-		System.out.println("--------------------------------");
-		for (Customer customer : repository.findByLastName("Smith")) {
-			System.out.println(customer);
-		}
-		System.out.println("Customers found with findByLastName('Chow'):");
-		System.out.println("--------------------------------");
-		for (Customer customer : repository.findByLastName("Chow")) {
-			System.out.println(customer);
-		}
-	}
+//	public void run(String... arg0) throws Exception {
+//		// TODO Auto-generated method stub
+//		repository.deleteAll();
+//
+//		// save a couple of customers
+//		repository.save(new Customer("Alice", "Smith"));
+//		repository.save(new Customer("Bob", "Smith"));
+//		repository.save(new Customer("Jack", "Chow"));
+//
+//		// fetch all customers
+//		System.out.println("Customers found with findAll():");
+//		System.out.println("-------------------------------");
+//		for (Customer customer : repository.findAll()) {
+//			System.out.println(customer);
+//		}
+//		System.out.println();
+//
+//		// fetch an individual customer
+//		System.out.println("Customer found with findByFirstName('Alice'):");
+//		System.out.println("--------------------------------");
+//		System.out.println(repository.findByFirstName("Alice"));
+//
+//		System.out.println("Customers found with findByLastName('Smith'):");
+//		System.out.println("--------------------------------");
+//		for (Customer customer : repository.findByLastName("Smith")) {
+//			System.out.println(customer);
+//		}
+//		System.out.println("Customers found with findByLastName('Chow'):");
+//		System.out.println("--------------------------------");
+//		for (Customer customer : repository.findByLastName("Chow")) {
+//			System.out.println(customer);
+//		}
+//	}
 
 }
